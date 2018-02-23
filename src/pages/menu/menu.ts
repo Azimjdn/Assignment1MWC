a0e6aa35fd8d2e0ac3f4383d66de5d26c0c3eaa1
Ain Syuhada <nws04_01098870@pb.edu.bn>
2018-02-23 23:03:13 +0800

    Added nav in menu page

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { GrabABoatPage } from '../grab-a-boat/grab-a-boat';
+import { WeatherForecastPage } from '../weather-forecast/weather-forecast';
+import { MapPage } from '../map/map';
+import { SchedulePage } from '../schedule/schedule';
 
 /**
  * Generated class for the MenuPage page.
@@ -16,6 +20,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class MenuPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public gotoGrabABoat(){
+    this.navCtrl.push(GrabABoatPage);
+  }
+
+  public gotoWeatherForecast(){
+    this.navCtrl.push(WeatherForecastPage);
+  }
+
+  public gotoMap(){
+    this.navCtrl.push(MapPage);
+  }
+
+  public gotoSchedule(){
+    this.navCtrl.push(SchedulePage);
   }
 
   ionViewDidLoad() {
